3c9305bd538b8ee0c084d6ed7cd291dd7f78a376
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-08-21 20:35:58 +0530

    feat: Integrate socket.io for real-time messaging and update chat functionality

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import Chatting from "../components/chat/Chatting";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import api from "../config/api";
+import socketAPI from "../config/webSocket";
 
 const Chat = () => {
   const navigate = useNavigate();
@@ -23,15 +24,22 @@ const Chat = () => {
   };
 
   // console.log(user);
-  const currentUser = 1;
+  //const currentUser = 1;
   console.log(recentUser);
 
   useEffect(() => {
     if (!isLogin) {
       navigate("/login");
-    } else {
+    }
+
+    if (isLogin && user) {
+      socketAPI.emit("OmBhramyaNamah", user._id);
       fetchRecentUsers();
     }
+
+    return () => {
+      socketAPI.emit("OmNamahShivay", user._id);
+    };
   }, []);
 
   return (
@@ -42,22 +50,22 @@ const Chat = () => {
             <h1>Recent Chats</h1>
 
             {recentUser.length > 0 &&
-              recentUser.map((user, idx) => (
+              recentUser.map((friend, idx) => (
                 <div
                   key={idx}
-                  onClick={() => (setSelectedFriend(user), setIsOpenChat(true))}
+                  onClick={() => {
+                    setSelectedFriend(friend);
+                    setIsOpenChat(true);
+                  }}
                   className="cursor-pointer"
                 >
-                  {user.fullName}
+                  {friend.fullName}
                 </div>
               ))}
           </div>
           <div className="w-14/17">
             {selectedFriend ? (
-              <Chatting
-                selectedFriend={selectedFriend}
-                currentUser={currentUser}
-              />
+              <Chatting selectedFriend={selectedFriend} currentUser={user} />
             ) : (
               <div>Select a Friend to start chat</div>
             )}
